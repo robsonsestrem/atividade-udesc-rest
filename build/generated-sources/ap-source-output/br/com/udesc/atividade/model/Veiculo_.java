5d94b63828ff4d47bb15b46dd5b714868054697c
Robson Sestrem <sestrem.robson@gmail.com>
2022-02-12 17:33:41 -0300

    ajustes git

--- build/generated-sources/ap-source-output/br/com/udesc/atividade/model/Veiculo_.java
+++ build/generated-sources/ap-source-output/br/com/udesc/atividade/model/Veiculo_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-11T15:55:59")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-05T17:02:56")
 @StaticMetamodel(Veiculo.class)
 public class Veiculo_ { 
 
